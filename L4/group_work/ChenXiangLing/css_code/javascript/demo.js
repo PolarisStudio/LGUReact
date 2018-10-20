0dc54ef3a17e6935db077cca8face5d1f3a54d81
XieGuochao <117010307@link.cuhk.edu.cn>
2018-10-20 15:02:02 +0800

    group_work4

---
+++ L4/group_work/ChenXiangLing/css_code/javascript/demo.js
@@ -0,0 +1,26 @@
+var a = 10, b = 20, c = 30;
+
+//max of three
+if (a>=b && a>=c){
+    max = a;
+}
+else if(b>=a && b>=c){
+    max = b;
+}
+else{
+    max = c;
+}
+
+console.log("Max is", max)
+
+//while sentence
+var t = 0;
+while (t<=100){
+    console.log(t);
+    t += 1;
+}
+
+//for sentence
+for (var i=1;i<=100;i+=1){
+    
+}
