d44a3ad292f71946a088e9212bc14fab319af801
XieGuochao <117010307@link.cuhk.edu.cn>
2018-11-04 00:15:27 +0800

    profile

--- L5/react_demo/profile/src/App.js
+++ L5/react_demo/profile/src/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class Portrait extends Component{
-  render(){
+var contactList = {
+  email: '117010307@link.cuhk.edu.cn',
+  address: "2001 Longxiang Road, Longgang District, Shenzhen",
+};
+
+class Portrait extends Component {
+  render() {
     return (
       <div className="portrait">
-        <img className='portrait-img'  src="https://www.ipolaris.club/media/img/8801111.jpg" alt="portrait"></img>
+        <div>
+        <div>
+          <img className='portrait-img' src="https://www.ipolaris.club/media/img/8801111.jpg" alt="portrait"></img>
+
         </div>
+        <div className='container-col'>
+          <em className='em-strong'>David</em>
+          <hr></hr>
+          <em>CUHK(SZ)</em>
+        </div>
+        </div>
+      </div>
     )
   }
 }
 
-class Header extends Component{
-  render(){
+class Header extends Component {
+  render() {
     return (
       <div className="header">
 
@@ -23,12 +37,59 @@ class Header extends Component{
   }
 }
 
+class MailIcon extends Component{
+  render(){
+    return (
+      <div className="Icon">
+        &#xe623;
+      </div>
+    )
+  }
+}
+
+class AddressIcon extends Component{
+  render(){
+    return (
+      <div className="Icon">
+        &#xe61d;
+      </div>
+    )
+  }
+}
+
+
+class Middle extends Component{
+  render(){
+    return (
+      <div className="Middle">
+        <a href={"mailto:"+contactList.email}>
+        <div className='container-col'>
+          <MailIcon></MailIcon>
+          <em>{contactList.email}</em>
+          
+        </div>
+        </a>
+
+        <div className='container-col'>
+          <AddressIcon></AddressIcon>
+          <em>{contactList.address}</em>
+          
+        </div>
+      </div>
+    )
+  }
+}
 class App extends Component {
   render() {
     return (
+      <div>
       <Header>
 
       </Header>
+      <Middle>
+
+      </Middle>
+      </div>
     );
   }
 }
@@ -36,21 +97,21 @@ class App extends Component {
 export default App;
 
 
-fetch(
-  "/api/weather/city/101280601", {
-      method: "GET",
-  }
-)
-.then(
-  function(response){
-      return response.json()
-  },
-  function(error){
-      
-  }
-)
-.then(
-  function(data){
-    console.log(data)
-  }
-)
+// fetch(
+//   "/api/weather/city/101280601", {
+//       method: "GET",
+//   }
+// )
+// .then(
+//   function(response){
+//       return response.json()
+//   },
+//   function(error){
+
+//   }
+// )
+// .then(
+//   function(data){
+//     console.log(data)
+//   }
+// )
