b066ce717cdf721827dc615f3d4429c9ea26a3e5
XieGuochao <117010307@link.cuhk.edu.cn>
2018-11-03 14:30:10 +0800

    fetch

--- L5/react_demo/profile/src/App.js
+++ L5/react_demo/profile/src/App.js
@@ -6,7 +6,7 @@ class Portrait extends Component{
   render(){
     return (
       <div className="portrait">
-        <img className='portrait-img'  src="https://www.ipolaris.club/media/img/8801111.jpg"></img>
+        <img className='portrait-img'  src="https://www.ipolaris.club/media/img/8801111.jpg" alt="portrait"></img>
         </div>
     )
   }
@@ -34,3 +34,23 @@ class App extends Component {
 }
 
 export default App;
+
+
+fetch(
+  "/api/weather/city/101280601", {
+      method: "GET",
+  }
+)
+.then(
+  function(response){
+      return response.json()
+  },
+  function(error){
+      
+  }
+)
+.then(
+  function(data){
+    console.log(data)
+  }
+)
