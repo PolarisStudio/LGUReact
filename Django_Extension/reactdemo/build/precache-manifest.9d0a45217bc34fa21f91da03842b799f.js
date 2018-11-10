3df2f7934426319814f3613dae859426f659bba6
XieGuochao <117010307@link.cuhk.edu.cn>
2018-11-10 14:17:52 +0800

    Django_react

---
+++ Django_Extension/reactdemo/build/precache-manifest.9d0a45217bc34fa21f91da03842b799f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "06756cb55280076229d8",
+    "url": "/static/js/main.06756cb5.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "06756cb55280076229d8",
+    "url": "/static/css/main.0a724912.chunk.css"
+  },
+  {
+    "revision": "2523711f6d7b06d36721e3744ee1c48a",
+    "url": "/index.html"
+  }
+];
